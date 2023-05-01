fef7fa1218ff87f085c532669f348e2764cd114c
Cam Parry <64690031+CamParry@users.noreply.github.com>
2023-05-01 02:33:39 +0200

    Added FontAwesome config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,12 @@ import { ConsoleChat } from '@/components/ConsoleChat';
 import { Layout } from '@/components/Layout';
 import { SiteProvider } from '@/contexts/Site';
 import '@/styles/global.css';
+import { config as fontAweConfig } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 import { DefaultSeo } from 'next-seo';
 import type { AppProps } from 'next/app';
+
+fontAweConfig.autoAddCss = false;
 
 const SEOConfig = {
 	titleTemplate: '%s | ' + process.env.NEXT_PUBLIC_SITE_TITLE
